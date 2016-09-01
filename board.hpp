f8d45477fd289dfa5bd2df29370fd850d9432a4f
RMUTime <rmuarjun588@gmail.com>
2016-08-31 20:58:10 -0400

    added class and member function in board.hpp

--- board.hpp
+++ board.hpp
@@ -1,3 +1,16 @@
 #ifndef __BOARD_H__
 #define __BOARD_H__
+
+#include <vector>
+
+class Board {
+	std::vector<std::vector<int>> board;
+	std::vector<int> history;
+	std::vector<int> numFilled;
+public:
+	Board();
+	bool checkWin();
+	void makeMove(int column, int turn);
+	bool isValid();
+};
 #endif
