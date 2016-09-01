b6b93c165b869d25b718fa220cd0938b3198c294
Neil Lohana <Neil_lohana@hotmail.com>
2016-09-01 17:18:38 -0400

    Finished checkWin in board.cpp

--- board.cpp
+++ board.cpp
@@ -24,4 +24,27 @@ bool Board::makeMove(int column, int turn) {
 	return true;
 }
 
-bool Board::checkWin(){ return true; }
+bool checkDirection(const std::vector<std::vector<int>> board, int a1, int a2, int b1, int b2, int c1, int c2, int d1, int d2) {
+	int a = board.at(a1).at(a2);
+	int b = board.at(b1).at(b2);
+	int c = board.at(c1).at(c2);
+	int d = board.at(d1).at(d2);
+
+	return ((a == 1 || a == 2) && a == b && a == c && a == d);
+}
+
+bool Board::checkWin(){
+	for(int i = 0; i < 6; ++i) {
+		for(int j = 0; j < 7; ++j) {
+			// Horizontal
+			if (checkDirection(board, i, j, i, j+1, i, j+2, i, j+3)) return true;
+			// Vertical
+			if (checkDirection(board, i, j, i+1, j, i+2, j, i+3, j)) return true;
+			// Diagonal Left
+			if (checkDirection(board, i, j, i+1, j+1, i+2, j+2, i+3, j+3)) return true;
+			// Diagonal Right
+			if (checkDirection(board, i, j, i+1, j-1, i+2, j-2, i+3, j-3)) return true;
+		}
+	}
+	return false;
+}
