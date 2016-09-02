79e6b3f7257b94f7193ef1eb0635d000a70428d4
Neil Lohana <Neil_lohana@hotmail.com>
2016-09-01 20:31:18 -0400

    Added out_of_range exception handling for board.at() in checkDirection

--- board.cpp
+++ board.cpp
@@ -1,4 +1,5 @@
 #include "board.hpp"
+#include <stdexcept>
 
 Board::Board() {
 	for (int i = 0; i < 6; ++i) {
@@ -25,12 +26,17 @@ bool Board::makeMove(int column, int turn) {
 }
 
 bool Board::checkDirection(int a1, int a2, int b1, int b2, int c1, int c2, int d1, int d2) {
-	int a = board.at(a1).at(a2);
-	int b = board.at(b1).at(b2);
-	int c = board.at(c1).at(c2);
-	int d = board.at(d1).at(d2);
+	try {
+		int a = board.at(a1).at(a2);
+		int b = board.at(b1).at(b2);
+		int c = board.at(c1).at(c2);
+		int d = board.at(d1).at(d2);
 
-	return ((a == 1 || a == 2) && a == b && a == c && a == d);
+		return ((a == 1 || a == 2) && a == b && a == c && a == d);
+	}
+	catch (const std::out_of_range& e) {
+		return false;
+	}
 }
 
 bool Board::checkWin(){
